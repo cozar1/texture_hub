9ce59ca064e9493ead7ec05853572071872a5fed
cozar1 <150320042+cozar1@users.noreply.github.com>
2026-04-30 17:20:41 +1200

    login

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 
 app = Flask(__name__)
+user_id = None
 
 def query(query, param=(), commit=False):
     with sqlite3.connect("database.db") as conn:
@@ -15,6 +16,57 @@ def query(query, param=(), commit=False):
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    global user_id
+
+    textures = query("SELECT * FROM Texture")
+    return render_template('home.html', textures=textures, user_id=user_id)
+
+@app.route('/signup',methods=["POST","GET"])
+def signup():
+    global user_id
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username and password:
+            usernames = query("SELECT user_name FROM user")
+            if username in usernames:
+                return app.redirect("/login")
+            else:
+                query('''INSERT INTO user (user_name, user_password, user_rating)
+                    VALUES (?, ?, ?)''',(username,password,0))
+                
+                _user_id = query("SELECT user_id FROM user WHERE user_name = ?",(username,))
+                user_id = _user_id
+
+            return app.redirect("/")
+
+
+    return render_template('login.html')
+
+@app.route('/login',methods=["POST","GET"])
+def login():
+    global user_id
+
+    if request.method == 'POST':
+        print("pos")
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if username and password:
+            usernames = query("SELECT user_name FROM User")
+            if (username,) in usernames:
+                print("username in usernames")
+                if password == query("SELECT user_password FROM User WHERE user_name = ?",(username,))[0][0]:
+                    print("Logged in as "+str(username))
+
+                    _user_id = query("SELECT user_id FROM user WHERE user_name = ?",(username,))
+                    user_id = _user_id
+
+                    return app.redirect("/")
+
+
+    return render_template('login.html')
 
 app.run(debug=True)
