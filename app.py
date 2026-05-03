9d9a2921dbf44647a77a9ddded90be580de18510
cozar1 <150320042+cozar1@users.noreply.github.com>
2026-05-04 08:10:58 +1200

    uploading textures

--- app.py
+++ app.py
@@ -1,30 +1,80 @@
-from flask import Flask, render_template, request
-import sqlite3
+import os
+import uuid
+
+from flask import Flask, render_template, request, redirect, session, abort
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
-user_id = None
+app.secret_key = 'replace-this-with-a-secure-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-def query(query, param=(), commit=False):
-    with sqlite3.connect("database.db") as conn:
-        cur = conn.cursor()
-        cur.execute(query, param)
-        if commit:
-            conn.commit()
-            return cur.lastrowid
-        else:
-            return cur.fetchall()
+db = SQLAlchemy(app)
+
+STATIC_IMAGES_DIR = os.path.join(app.root_path, 'static', 'images')
+DEFAULT_TEXTURE_URL = '/static/images/texture.png'
+ALLOWED_IMAGE_EXTENSIONS = {'.png', '.jpg', '.jpeg', '.gif', '.webp'}
+
+
+def get_current_user():
+    user_id = session.get('user_id')
+    if user_id is None:
+        return None
+    return User.query.get(user_id)
+
+
+@app.template_filter('texture_url')
+def texture_image_url(address):
+    """Serve DB path under static/images/; fall back if file missing."""
+    if not address:
+        return DEFAULT_TEXTURE_URL
+    addr = str(address).strip().replace('\\', '/')
+    if addr.startswith('/static/images/'):
+        rel = addr[len('/static/images/') :]
+    elif addr.startswith('static/images/'):
+        rel = addr[len('static/images/') :]
+    else:
+        rel = os.path.basename(addr)
+
+    rel = os.path.basename(rel)
+    if not rel or rel.startswith('.'):
+        return DEFAULT_TEXTURE_URL
+
+    full = os.path.join(STATIC_IMAGES_DIR, rel)
+    if os.path.isfile(full):
+        return f'/static/images/{rel}'
+    return DEFAULT_TEXTURE_URL
+
+
+# --- Models ---
+
+class User(db.Model):
+    __tablename__ = 'User'
+    user_id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(80), unique=True, nullable=False)
+    user_password = db.Column(db.String(120), nullable=False)
+    user_rating = db.Column(db.Integer, default=0)
+
+class Texture(db.Model):
+    __tablename__ = 'Texture'
+    texture_id = db.Column(db.Integer, primary_key=True)
+    texture_name = db.Column(db.String(80), unique=True, nullable=False)
+    texture_address = db.Column(db.String(120), nullable=False)
+    texture_user_id = db.Column(db.Integer, default=0)
+
+
+
+# --- Routes ---
 
 @app.route('/')
 def home():
-    global user_id
+    textures = Texture.query.all()
+    return render_template('home.html', user=get_current_user(), textures=textures)
 
-    textures = query("SELECT * FROM Texture")
-    user_name = query("SELECT user_name FROM user WHERE user_id = ?",(user_id,))[0][0]
-    return render_template('home.html', textures=textures, user_name=user_name)
 
-@app.route('/signup',methods=["POST","GET"])
+@app.route('/signup', methods=["POST", "GET"])
 def signup():
-    global user_id
     error = None
 
     if request.method == 'POST':
@@ -32,48 +82,116 @@ def signup():
         password = request.form.get('password')
 
         if username and password:
-            usernames = query("SELECT user_name FROM user")
-            if (username,) in usernames:
+            if User.query.filter_by(user_name=username).first():
                 error = "There is Already an Account with this Username"
+            elif User.query.filter_by(user_password=password).first():
+                existing = User.query.filter_by(user_password=password).first()
+                error = f"User {existing.user_name} Already has this password"
             else:
-                query('''INSERT INTO user (user_name, user_password, user_rating)
-                    VALUES (?, ?, ?)''',(username,password,0))
-                
-                _user_id = query("SELECT user_id FROM user WHERE user_name = ?",(username,))[0][0]
-                user_id = _user_id
-
-                return app.redirect("/")
-
+                new_user = User(user_name=username, user_password=password, user_rating=0)
+                db.session.add(new_user)
+                db.session.commit()
+                session['user_id'] = new_user.user_id
+                return redirect("/")
+        else:
+            error = "Please enter a Username & Password"
 
     return render_template('signup.html', error=error)
 
-@app.route('/login',methods=["POST","GET"])
+
+@app.route('/login', methods=["POST", "GET"])
 def login():
-    global user_id
     error = None
 
     if request.method == 'POST':
-        print("pos")
         username = request.form.get('username')
         password = request.form.get('password')
 
         if username and password:
-            usernames = query("SELECT user_name FROM User")
-            if (username,) in usernames:
-                if password == query("SELECT user_password FROM User WHERE user_name = ?",(username,))[0][0]:
-                    print("Logged in as "+str(username))
-
-                    _user_id = query("SELECT user_id FROM user WHERE user_name = ?",(username,))[0][0]
-                    user_id = _user_id
-
-                    return app.redirect("/")
+            _user = User.query.filter_by(user_name=username).first()
+            if _user:
+                if password == _user.user_password:
+                    print("Logged in as " + username)
+                    session['user_id'] = _user.user_id
+                    return redirect("/")
                 else:
                     error = "Username or Password is Incorrect"
-
             else:
                 error = "Username isn't Registered with any Account"
-
+        else:
+            error = "Please enter a Username & Password"
 
     return render_template('login.html', error=error)
 
-app.run(debug=True)
+@app.route('/logout', methods=["POST", "GET"])
+def logout():
+    session['user_id'] = None
+    return redirect("/")
+
+
+@app.route('/user/<username>')
+def user_profile(username):
+    profile_user = User.query.filter_by(user_name=username).first()
+    if profile_user is None:
+        abort(404)
+
+    textures = Texture.query.filter_by(texture_user_id=profile_user.user_id).all()
+
+    return render_template(
+        'user.html',
+        user=get_current_user(),
+        profile_user=profile_user,
+        follower_count=367,
+        joined_display='1/05/2026',
+        texture_count_display='3,546',
+        collection_count_display='742',
+        texture_more_total=13357,
+        collection_more_total=13357,
+        textures=textures,
+    )
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    user = get_current_user()
+    if user is None:
+        return redirect('/login')
+
+    error = None
+
+    if request.method == 'POST':
+        display_name = (request.form.get('display_name') or '').strip()
+        file = request.files.get('image')
+
+        if not display_name:
+            error = 'Please enter a display name.'
+        elif not file or file.filename == '':
+            error = 'Please choose an image file.'
+        elif Texture.query.filter_by(texture_name=display_name).first():
+            error = 'That display name is already taken.'
+        else:
+            original = secure_filename(file.filename)
+            ext = os.path.splitext(original)[1].lower()
+            if ext not in ALLOWED_IMAGE_EXTENSIONS:
+                error = 'Allowed types: PNG, JPEG, GIF, WebP.'
+            else:
+                os.makedirs(STATIC_IMAGES_DIR, exist_ok=True)
+                stored_name = f'{uuid.uuid4().hex}{ext}'
+                dest = os.path.join(STATIC_IMAGES_DIR, stored_name)
+                file.save(dest)
+                url_path = f'/static/images/{stored_name}'
+                texture = Texture(
+                    texture_name=display_name,
+                    texture_address=url_path,
+                    texture_user_id=user.user_id,
+                )
+                db.session.add(texture)
+                db.session.commit()
+                return redirect('/')
+
+    return render_template('upload.html', user=user, error=error)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
